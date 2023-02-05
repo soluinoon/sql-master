1ee1be8fbdc71fc162431f1b7030f81e933d0a77
Jihong Kim <kingjihong@naver.com>
2023-02-05 13:01:23 +0900

    add: select '강원도에 위치한 생상공장 목록 출력하기'

---
+++ select/sql131112.sql
@@ -0,0 +1,3 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/131112
+SELECT FACTORY_ID, FACTORY_NAME, ADDRESS FROM FOOD_FACTORY
+WHERE SUBSTRING_INDEX(ADDRESS, " ", 1) = "강원도" ORDER BY FACTORY_ID;
